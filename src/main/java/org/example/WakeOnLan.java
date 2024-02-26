bcebcadce2de1e2b88e74ab28eb76fcfae7864f1
Tomas Duchek <neb7pxuk@duchic.anonaddy.com>
2024-02-26 20:48:06 +0100

    add byte arrays

--- src/main/java/org/example/WakeOnLan.java
+++ src/main/java/org/example/WakeOnLan.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.net.InetAddress;
+
 public class WakeOnLan {
 
     public static final int PORT = 9;
@@ -15,6 +17,14 @@ public class WakeOnLan {
 
         try {
             byte[] macBytes = getMacBytes(macAddress);
+            byte[] bytes = new byte[6 + 16 * macBytes.length];
+            for (int i = 0; i < 6; i++) {
+                bytes[i] = (byte) 0xff;
+            }
+            for (int i = 6; i < bytes.length; i += macBytes.length) {
+                System.arraycopy(macBytes, 0, bytes, i, macBytes.length);
+            }
+
             //TODO
 
         } catch (Exception e) {
